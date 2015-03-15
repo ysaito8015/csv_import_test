5d5a8d846b83bb82b1a40321f6fe2b23e9102063
Yusuke Saito <ysaito8015@gmail.com>
2015-03-15 18:30:11 +0900

    Amend Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,21 +8,10 @@ class Product < ActiveRecord::Base
       row = Hash[[header, spreadsheet.row(i)].transpose]
 
       product = find_by(id: row["id"]) || new
-      product.attributes row.to_hash.slice(*updatable_attributes)
+      product.attributes = row.to_hash.slice(*updatable_attributes)
       product.save!
     end
   end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when '.csv'  then Roo::Csv.new(file.path, nil, :ignore)
-    when '.xls'  then Roo::Excel.new(file.path, nil, :ignore)
-    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
-    when '.ods'  then Roo::OpenOffice.new(file.path, nil, :ignore)
-    else raise "Unknown file type: #{file.original_filename}"
-    end
-  end
-
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
@@ -35,7 +24,6 @@ class Product < ActiveRecord::Base
   end
 
   def self.updatable_attributes
-    ["name", "price", "releaded_on"]
-    #%w(name price released_on)
+    %w(name price released_on)
   end
 end
