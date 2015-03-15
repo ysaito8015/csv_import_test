e5249bc3d2d32ad00e64ea67d920d532b64fbb48
Yusuke Saito <ysaito8015@gmail.com>
2015-03-15 18:24:29 +0900

    Add xls, xlsx, ods importer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,41 @@
 class Product < ActiveRecord::Base
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      product =find_by(id: row["id"]) || new
-      product.attributes = row.to_hash.slice(*updatable_attributes)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+
+      product = find_by(id: row["id"]) || new
+      product.attributes row.to_hash.slice(*updatable_attributes)
       product.save!
     end
   end
 
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv'  then Roo::Csv.new(file.path, nil, :ignore)
+    when '.xls'  then Roo::Excel.new(file.path, nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    when '.ods'  then Roo::OpenOffice.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv'  then Roo::Csv.new(file.path,    nil, :ignore)
+    when '.xls'  then Roo::Excel.new(file.path,  nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    when '.ods'  then Roo::OpenOffice.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
   def self.updatable_attributes
-    %w(name price released_on)
+    ["name", "price", "releaded_on"]
+    #%w(name price released_on)
   end
 end
